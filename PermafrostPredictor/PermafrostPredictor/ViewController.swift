f7ac363abe1690b00847c971cdc2220cd9ac6683
Dev-Laurin <lafisher@alaska.edu>
2018-02-28 10:25:40 -0900

    Have a line draggable and bounded, with constraints.

--- PermafrostPredictor/PermafrostPredictor/ViewController.swift
+++ PermafrostPredictor/PermafrostPredictor/ViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var line: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //To recognize a pan gesture (dragging) on a view (our lines in the UI)
+    @IBAction func handleLineGesture(recognizer:UIPanGestureRecognizer){
+        //get the translation movement from the recognizer
+        let translation = recognizer.translation(in: self.view)
+        //move the view
+        if let view = recognizer.view{
+            var yVal = view.center.y + translation.y
+            
+            //Bounding
+            let bottomConstraint = self.view.frame.height-40
+            if((view.center.y + translation.y) < 40){
+                yVal = 40
+            }
+            else if( (view.center.y + translation.y) > bottomConstraint){
+                yVal = bottomConstraint
+            }
+            
+            //Set the coordinates
+            view.center = CGPoint(x:view.center.x, //only move vertically, don't change x 
+                                  y:yVal)
+        }
+        //Don't have image keep moving, set translation to zero because we are done 
+        recognizer.setTranslation(CGPoint.zero, in: self.view)
     }
 
     override func didReceiveMemoryWarning() {
