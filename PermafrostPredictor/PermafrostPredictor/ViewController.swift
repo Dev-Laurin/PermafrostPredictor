7be919db77e3d3b3356ef14c0c53cc104cf1b116
Dev-Laurin <lafisher@alaska.edu>
2018-03-04 09:10:25 -0900

    Image resizes correctly with line movement. Adding stack view next.

--- PermafrostPredictor/PermafrostPredictor/ViewController.swift
+++ PermafrostPredictor/PermafrostPredictor/ViewController.swift
@@ -10,35 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
     @IBOutlet weak var line: UIImageView!
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var stackView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+
+    //IDEA: TODO re-write this function to have it use a stack view owning the gesture recognizer
+        //This allows the stack view to change the height of the 1st view (line) and
+        //resize the 2nd view (image) accordingly
     
     //To recognize a pan gesture (dragging) on a view (our lines in the UI)
-    @IBAction func handleLineGesture(recognizer:UIPanGestureRecognizer){
+    @IBAction func handleGesture(recognizer:UIPanGestureRecognizer){
+      
         //get the translation movement from the recognizer
         let translation = recognizer.translation(in: self.view)
         //move the view
         if let view = recognizer.view{
             var yVal = view.center.y + translation.y
-            
+
             //Bounding
             let bottomConstraint = self.view.frame.height-40
-            if((view.center.y + translation.y) < 40){
-                yVal = 40
+            let topConstraint = CGFloat(60)
+            
+            if(yVal < topConstraint){
+                yVal = topConstraint
             }
-            else if( (view.center.y + translation.y) > bottomConstraint){
+            else if( yVal > bottomConstraint){
                 yVal = bottomConstraint
             }
+            else{
+                //Set the coordinates
+                view.center = CGPoint(x:view.center.x, //only move vertically, don't change x
+                                      y:yVal)
+            }
             
-            //Set the coordinates
-            view.center = CGPoint(x:view.center.x, //only move vertically, don't change x 
-                                  y:yVal)
+           // var imageHeight = img.frame.height - translation.y
+            let screenSize : CGRect = UIScreen.main.bounds
+            let screenHeight = screenSize.height
+
+            img.frame = CGRect(origin: CGPoint(x: view.center.x - img.frame.width/2, y: yVal), size: CGSize(width: img.frame.width,height: screenHeight - yVal))
         }
-        //Don't have image keep moving, set translation to zero because we are done 
+        //Don't have image keep moving, set translation to zero because we are done
         recognizer.setTranslation(CGPoint.zero, in: self.view)
     }
 
